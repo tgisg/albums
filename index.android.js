9f887fdfc6e6a80ba14610cebf2a2000473afa09
tgisg <soniagupta504@gmail.com>
2017-02-06 20:24:25 -0700

    Begin button component

--- index.android.js
+++ index.android.js
@@ -2,15 +2,19 @@
 
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 // when you make your own module you have to give relative path
 // we might have modules named the same thing, packager needs to know which is which
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
   // below is a self closing component, you are not adding text to your header component
-  <Header headerText={'Albums'} />
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
   );
 
 // Render it to the device
