3d85f14a4d9f9d77f567dfc557a31895a0c1ce20
tgisg <soniagupta504@gmail.com>
2017-02-19 18:32:08 -0700

    add facebook sdk

--- android/app/src/main/java/com/albums/MainApplication.java
+++ android/app/src/main/java/com/albums/MainApplication.java
@@ -3,12 +3,18 @@ package com.albums;
 import android.app.Application;
 import android.util.Log;
 
+
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
